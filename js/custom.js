735f10973a46156ffc9bb6a97e2038fb62b8e890
satunaite <eglesatunaite@gmail.com>
2020-11-10 20:29:11 +0200

    add landing page styling

--- js/custom.js
+++ js/custom.js
@@ -38,13 +38,13 @@
   });
 
   //jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
+  /*$(window).scroll(function() {
     if ($(".navbar-default").offset().top > 50) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
       $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-  });
+  });*/
 
   // Testimonials Slider
   $('.bxslider').bxSlider({
