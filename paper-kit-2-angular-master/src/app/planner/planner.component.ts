dd1f2d130a825f7bba1949dc392b7529c4ed55a5
satunaite <eglesatunaite@gmail.com>
2020-12-09 18:57:57 +0200

    finish travel planner page

--- paper-kit-2-angular-master/src/app/planner/planner.component.ts
+++ paper-kit-2-angular-master/src/app/planner/planner.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbdModalContent } from "./results.modal";
 
 @Component({
-  selector: 'app-planner',
-  templateUrl: './planner.component.html',
-  styleUrls: ['./planner.component.css']
+  selector: "app-planner",
+  templateUrl: "./planner.component.html",
+  styleUrls: ["./planner.component.css"],
 })
 export class PlannerComponent implements OnInit {
-
+  userOptions: number[] = [];
+  packageNumber: number;
   alienBelief = 50;
+  displayResults = false;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  pickRandomPackage(): void {
+    if (!this.userOptions.length) {
+      this.packageNumber = 0;
+      this.displayResults = true;
+      return;
+    }
+
+    this.packageNumber = this.userOptions[
+      Math.floor(Math.random() * this.userOptions.length)
+    ];
+
+    this.displayResults = true;
   }
 
+  updateUserOptions(preference: number) {
+    this.userOptions.push(preference);
+  }
+
+  open(name: string) {
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.name = name;
+  }
 }
