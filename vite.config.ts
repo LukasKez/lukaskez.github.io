a5cc18aa0b2e77f7a2c4bda5d0dc4a048e49c5c1
Lukas <lkezevicius@gmail.com>
2023-10-14 02:14:37 +0300

    script to deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name].[hash].js',
+        chunkFileNames: 'js/[name].[hash].js',
+        assetFileNames: 'media/[name].[hash].[ext]',
+      },
+    },
+  },
+
 })
